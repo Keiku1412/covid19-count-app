4726972a5041c96e5fb98e6871332dd7120f2701
Keiku1412 <61989780+Keiku1412@users.noreply.github.com>
2023-04-11 22:22:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<img src="https://raw.githubusercontent.com/hifiaz/covid19/master/lib/images/icon.png " width="50">
+
+# covid19
+
+Monitoring Covid.
+
+<img src="https://raw.githubusercontent.com/hifiaz/covid19/master/flutter_02.png" width="250">
+<img src="https://raw.githubusercontent.com/hifiaz/covid19/master/flutter_03.png" width="250">
+<img src="https://raw.githubusercontent.com/hifiaz/covid19/master/flutter_04.png" width="250">
+
+
+
+## Resource
+
+- [Rest API](https://github.com/mathdroid/covid-19-api)
+- [Kasus Corona API](https://louislugas.github.io/covid_19_cluster/json/kasus-corona-indonesia.json)
+- [Provider](https://pub.dev/packages/provider)
+- [http](https://pub.dev/packages/http)
+- [intl](https://pub.dev/packages/intl)
+- [flutter_datetime_picker](https://pub.dev/packages/flutter_datetime_picker)
+- [url_launcher](https://pub.dev/packages/url_launcher)
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:covid19/providers/country_provider.dart';
+
+import 'package:covid19/providers/history_provider.dart';
+import 'package:covid19/providers/home_provider.dart';
+import 'package:covid19/providers/province_provider.dart';
+import 'package:covid19/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => HomeProvider()),
+
+      ChangeNotifierProvider(create: (_) => ProvinceProvider()),
+      ChangeNotifierProvider(create: (_) => CountryProvider()),
+      ChangeNotifierProvider(create: (_) => HistoryProvider()),
+    ], child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid19',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
